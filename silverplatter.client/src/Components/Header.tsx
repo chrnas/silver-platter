827f21c23f669f8648ef5c0ef252a653ca5c555d
marhe598 <marhe598@student.liu.se>
2025-11-30 17:45:15 +0100

    Basic framework for landing page made

---
+++ silverplatter.client/src/Components/Header.tsx
@@ -0,0 +1,15 @@
+
+
+function Header() {
+    return (
+        <div style={{
+            width: "100vw",
+            height: "5rem",
+            backgroundColor: "red"
+        }}>
+            THIS IS THE HEADER
+        </div>
+    )
+}
+
+export default Header;
